ca78dd111dd8210043945d2a34714a02ce44d6f8
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-07-26 15:21:22 -0700

    add popup

--- src/component/studentComponent.js
+++ src/component/studentComponent.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
-import { Button, ButtonGroup } from "react-bootstrap";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import "../student.css";
+import {
+  Button,
+  ButtonGroup,
+  Navbar,
+  Container,
+  Modal,
+  InputGroup,
+  Form,
+} from "react-bootstrap";
+import {
+  faEdit,
+  faTrash,
+  faUser,
+  faSave,
+} from "@fortawesome/free-solid-svg-icons";
+import { AiOutlineClose } from "react-icons/ai";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class StudentComponent extends React.Component {
@@ -10,7 +25,15 @@ export default class StudentComponent extends React.Component {
     super(props);
     this.state = {
       students: [],
+      show: false,
     };
+  }
+
+  handleModal() {
+    this.setState({ show: true });
+  }
+  cutModal() {
+    this.setState({ show: false });
   }
 
   componentDidMount() {
@@ -21,42 +44,97 @@ export default class StudentComponent extends React.Component {
   }
 
   render() {
+    const { show } = this.state;
     return (
       <div>
-        <h1 className="text-center"> Student Management System</h1>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">
+              <FontAwesomeIcon icon={faUser} />
+              React Bootstrap
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <div className="container">
+          <h1 className="text-center "> Student Management System</h1>
 
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <td>Student ID</td>
-              <td>Student Name</td>
-              <td>Course</td>
-              <td>Course fee(Rs.)</td>
-              <td>Action</td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.students.map((student) => (
-              <tr key={student.id}>
-                <td>{student.id}</td>
-                <td>{student.name}</td>
-                <td>{student.course}</td>
-                <td>{student.fee}</td>
-
-                <td>
-                  <ButtonGroup>
-                    <Button size="sm" variant="outline-primary">
-                      <FontAwesomeIcon icon={faEdit} />
-                    </Button>
-                    <Button size="sm" variant="outline-danger">
-                      <FontAwesomeIcon icon={faTrash} />
-                    </Button>
-                  </ButtonGroup>
-                </td>
+          <table className="table table-striped tabl">
+            <thead className="thead-dark head">
+              <tr>
+                <td>Student ID</td>
+                <td>Student Name</td>
+                <td>Course</td>
+                <td>Course fee(Rs.)</td>
+                <td>Action</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.students.map((student) => (
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.course}</td>
+                  <td>{student.fee}</td>
+
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary">
+                        <FontAwesomeIcon icon={faEdit} />
+                      </Button>
+                      <Button size="sm" variant="outline-danger">
+                        <FontAwesomeIcon icon={faTrash} />
+                      </Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Button
+            onClick={() => this.handleModal()}
+            size="sm"
+            variant="outline-primary"
+            className="add"
+          >
+            <FontAwesomeIcon icon={faSave} /> Add Student
+          </Button>
+        </div>
+        <Modal
+          show={show}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Header>
+            <Modal.Title id="example-custom-modal-styling-title">
+              Question 1
+            </Modal.Title>
+            <AiOutlineClose onClick={() => this.cutModal()} />
+          </Modal.Header>
+          <Modal.Body>
+            <InputGroup className="mb-3">
+              <Form.Control
+                className="p-4 input-color-popup"
+                type="text"
+                name="Quiz Header"
+                placeholder="Write the Question 1"
+                id="Quiz Header"
+              />
+            </InputGroup>
+            <h4 className="text-popup mb-3 text-lg-left text-md-left">
+              Select Correct Answer
+            </h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="contained"
+              color="primary"
+              size="medium"
+              className="background-primary"
+            >
+              ADD
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
