ad34bd045e5ab46ac3da115055ffd2c33fb72b54
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-07-25 23:30:01 -0700

    add service file js

---
+++ src/component/studentComponent.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import studentService from '../services/studentService';
+
+class studentComponent extends React.Component
---
+++ src/services/studentService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const USERS_REST_API_URL= "http://localhost:8080/students";
+
+class studentService{
+    getStudents(){
+        axios.get(USERS_REST_API_URL);
+    }
+}
+export default new studentService()
